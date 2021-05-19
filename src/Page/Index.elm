34672ce12f3d7d899ccd88c96725b120374702fe
Dillon Kearns <dillon@dillonkearns.com>
2021-05-19 11:26:52 -0700

    Add scaffolding for elm-pages v2 app with submodule repo.

---
+++ src/Page/Index.elm
@@ -0,0 +1,70 @@
+module Page.Index exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import Document exposing (Document)
+import Head
+import Head.Seo as Seo
+import Html exposing (text)
+import Page exposing (Page, StaticPayload)
+import Pages.ImagePath as ImagePath
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    {}
+
+
+page : Page RouteParams Data
+page =
+    Page.singleRoute
+        { head = head
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+data : DataSource Data
+data =
+    DataSource.succeed ()
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = ImagePath.build [ "TODO" ]
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+type alias Data =
+    ()
+
+
+view :
+    StaticPayload Data RouteParams
+    -> Document Msg
+view static =
+    { title = "Hi!"
+    , body =
+        [ text "Hey there!"
+        ]
+    }
