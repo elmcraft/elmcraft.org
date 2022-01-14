a61a215481be32b5d9fe27c05c60de1ff3b4fbfa
Mario Rogic <hello@mario.net.au>
2022-01-14 20:02:06 +0000

    Try reduce font loading by having the @font-family include as part of static page content

--- src/Theme.elm
+++ src/Theme.elm
@@ -29,64 +29,82 @@ view x toWrapperMsg model static =
         , width fill
         ]
     <|
-        column
-            [ width fill
-            , spacing 20
-            ]
-            [ Theme.Navigation.navigation model static.route
-                |> Element.map toWrapperMsg
+        column [ width fill ]
+            [ manualCss
+            , column
+                [ width fill
+                , spacing 20
+                ]
+                [ Theme.Navigation.navigation model static.route
+                    |> Element.map toWrapperMsg
 
-            -- , el [ width fill ] <| html <| Html.pre [] [ Html.text <| Debug.toString x ]
-            , if static.route == Route.SPLAT__ { splat = [] } then
-                none
+                -- , el [ width fill ] <| html <| Html.pre [] [ Html.text <| Debug.toString x ]
+                , if static.route == Route.SPLAT__ { splat = [] } then
+                    none
 
-              else
-                standardCenteredSectionAdaptiveAt
-                    Theme.Layout.maxWidth
+                  else
+                    standardCenteredSectionAdaptiveAt
+                        Theme.Layout.maxWidth
+                        model
+                        white
+                        []
+                        [ heading1 static.title
+                        , el [ Background.color grey, width fill, height (px 2) ] none
+                        , let
+                            statusToString status =
+                                case status of
+                                    Seedling ->
+                                        "Seedling 🌱"
+
+                                    Budding ->
+                                        "Budding \u{1FAB4}"
+
+                                    Evergreen ->
+                                        "Evergreen 🌳"
+                          in
+                          case static.status of
+                            Just status ->
+                                row [ width fill ]
+                                    [ el [ paddingXY 0 10, width fill ] <|
+                                        paragraph [ Font.color charcoalLight, Font.size 14 ] <|
+                                            [ prefetchLink [ Font.color <| fromHex "#98B68F" ] { url = "/about/markers", label = text <| statusToString status }
+                                            , text <| " Planted " ++ format static.timestamps.created ++ " - Last tended " ++ format static.timestamps.updated
+                                            ]
+                                    , if not static.published && model.isDev then
+                                        el [ Background.color elmcraftNude ] <| text "not published"
+
+                                      else
+                                        none
+                                    ]
+
+                            Nothing ->
+                                -- For now, if we don't have a status, don't show dates either (covers us for pages like indexes where it's a bit odd?)
+                                none
+                        ]
+                        |> Element.map toWrapperMsg
+                , standardCenteredSection
                     model
                     white
-                    []
-                    [ heading1 static.title
-                    , el [ Background.color grey, width fill, height (px 2) ] none
-                    , let
-                        statusToString status =
-                            case status of
-                                Seedling ->
-                                    "Seedling 🌱"
-
-                                Budding ->
-                                    "Budding \u{1FAB4}"
-
-                                Evergreen ->
-                                    "Evergreen 🌳"
-                      in
-                      case static.status of
-                        Just status ->
-                            row [ width fill ]
-                                [ el [ paddingXY 0 10, width fill ] <|
-                                    paragraph [ Font.color charcoalLight, Font.size 14 ] <|
-                                        [ prefetchLink [ Font.color <| fromHex "#98B68F" ] { url = "/about/markers", label = text <| statusToString status }
-                                        , text <| " Planted " ++ format static.timestamps.created ++ " - Last tended " ++ format static.timestamps.updated
-                                        ]
-                                , if not static.published && model.isDev then
-                                    el [ Background.color elmcraftNude ] <| text "not published"
-
-                                  else
-                                    none
-                                ]
-
-                        Nothing ->
-                            -- For now, if we don't have a status, don't show dates either (covers us for pages like indexes where it's a bit odd?)
-                            none
-                    ]
+                    [ Region.mainContent ]
+                    static.content
+                , Theme.Footer.view model
                     |> Element.map toWrapperMsg
-            , standardCenteredSection
-                model
-                white
-                [ Region.mainContent ]
-                static.content
-            , Theme.Footer.view model
-                |> Element.map toWrapperMsg
+                ]
+            ]
+
+
+manualCss =
+    html <|
+        Html.node "style"
+            []
+            [ Html.text <|
+                """
+            @import url('https://rsms.me/inter/inter.css');
+            html, body { font-family: 'Inter', system-ui, sans-serif; width: 100%; }
+            @supports (font-variation-settings: normal) {
+              html, body { font-family: 'Inter var', system-ui, sans-serif; }
+            }
+            """
             ]
 
 
